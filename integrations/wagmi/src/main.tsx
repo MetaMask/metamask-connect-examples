19c52b85e05dafd53a4dda048c3b70775a18776a
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2026-03-19 16:38:36 +0530

    nit

--- integrations/wagmi/src/main.tsx
+++ integrations/wagmi/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { config } from './wagmi.ts'
+import { config } from './wagmi.config.ts'
 import App from './App.tsx'
 import './index.css'
 
